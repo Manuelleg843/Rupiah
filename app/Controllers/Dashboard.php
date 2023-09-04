8a175ba8ed82558bb6589f6d7dc5d63f077f3e9d
emanuellega843 <221910843@stis.ac.id>
2023-09-04 11:01:14 +0700

    mengubah routes

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -9,5 +9,10 @@ class Dashboard extends BaseController
     public function index()
     {
         //
+        echo view('layouts/header');
+        echo view('layouts/navbar');
+        echo view('layouts/sidebar');
+        // echo view('dashboard/index');
+        echo view('layouts/footer');
     }
 }
