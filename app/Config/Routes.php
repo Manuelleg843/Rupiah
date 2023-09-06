9871a1273e5098c9b4c2527a00b4ed46188399f1
intn <intan.trihandini@yahoo.com>
2023-09-06 08:20:21 +0700

    menambahkan folder assets, file di folder controller dan view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,17 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+#------------------------------------#
+#        Menu Upload Data            #
+#------------------------------------#
+$routes->get('/pdrb_upload', 'uploadData::index/uploadAngkaPDRB');
+
+
+
+#------------------------------------#
+#        Menu Tabel PDRB             #
+#------------------------------------#
+$routes->get('/tabel_ringkasan', 'tabelPDRB::index/tabelRingkasan');
+$routes->get('/tabel_perKota', 'tabelPDRB::index/tabelPerKota');
+$routes->get('/tabel_historiPutaran', 'tabelPDRB::index/tabelPutaran');
